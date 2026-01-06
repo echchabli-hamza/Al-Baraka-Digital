353a4e07490657c352b9efa7a4195e07729594f0
Hamza <echchablihamza1@gmail.com>
2026-01-06 11:40:11 -0800

    ajout de cors config

--- src/main/java/com/AlBaraka/demo/config/AppConfig.java
+++ src/main/java/com/AlBaraka/demo/config/AppConfig.java
@@ -5,6 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 
 @Configuration
@@ -14,6 +19,31 @@ public class AppConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+
+
+    @Configuration
+    public class CorsConfig {
+
+        @Bean
+        public CorsFilter corsFilter() {
+            CorsConfiguration config = new CorsConfiguration();
+
+            config.setAllowCredentials(true);
+            config.setAllowedOrigins(List.of(
+                    "http://localhost:4200",
+                    "http://161.35.220.69:4200"
+            ));
+            config.setAllowedHeaders(List.of("*"));
+            config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", config);
+
+            return new CorsFilter(source);
+        }
+    }
+
 
 
 }
